fe823e71b015b227c31805f0b30091c6f8b45dff
Abir <abir.pal899@gmail.com>
2022-07-16 22:41:06 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const args = process.argv.slice(2);
+
+const flagParser = () => {
+        let [i, flagIndex, totalFlags] = [0,0,0]
+        let currentflagValue = ""
+        let flagsObject = {}
+        for(i = 0; i < args.length; i++){
+                if(args[i].trim().length === 0){
+                        continue;
+                }
+
+                if(args[i][0] != '-'){
+                        currentflagValue += args[i];
+                        flagsObject[args[flagIndex]] = currentflagValue;
+                        currentflagValue = "";
+                        continue;
+                }
+
+                flagIndex = i;
+        }
+        return flagsObject;
+}
+module.exports = flagParser;
